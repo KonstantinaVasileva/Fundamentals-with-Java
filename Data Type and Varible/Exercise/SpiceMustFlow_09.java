3a758a879389ea6e8a032c69c0d80f81379be3c8
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2023-12-08 14:40:12 +0200

    Add files via upload
    9.  Spice Must Flow 
    Write a program that calculates the total amount of spice extracted from a source. 
    The source has a starting yield, which indicates how much spice can be mined on the first day. After it has been mined for a day, the yield drops by 10, meaning on the second day, it'll produce 10 less spice than on the first, on the third day 10 less than on the second, and so on (see examples). 
    A source is considered profitable only while its yield is at least 100 – when less than 100 spices are expected in a day, abandon the source. 
    The mining crew consumes 26 spices every day at the end of their shift and an additional 26 after the mine has been exhausted. Note that the workers cannot consume more spice than there is in storage. 
    When the operation is complete, print on the console on two separate lines how many days the mine has operated and the total amount of spice extracted.

---
+++ Data Type and Varible/Exercise/SpiceMustFlow_09.java
@@ -0,0 +1,23 @@
+package DataTypesAndVariablesExercise;
+
+import java.util.Scanner;
+
+public class SpiceMustFlow_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startingYield = Integer.parseInt(scanner.nextLine());
+        int allSpice = 0;
+        int count = 0;
+
+        while (startingYield >= 100){
+            allSpice += startingYield - 26;
+            startingYield -= 10;
+            count ++;
+        }
+        System.out.println(count);
+     //   if (allSpice>=26)
+            allSpice -= 26;
+        System.out.println(allSpice);
+    }
+}
