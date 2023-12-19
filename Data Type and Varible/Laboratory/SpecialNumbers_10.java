4107b2bc6e1e7af3a11fd6fc6bd72de194a86c07
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2023-12-19 23:47:06 +0200

    Add files via upload
    10.	Special Numbers
    A number is special when its sum of digits is 5, 7, or 11.
    Write a program to read an integer n and for all numbers in the range 1…n to print the number and if it is special or not (True / False).

---
+++ Data Type and Varible/Laboratory/SpecialNumbers_10.java
@@ -0,0 +1,25 @@
+package DataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class SpecialNumbers_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            int sum = 0;
+            while (num != 0){
+                int tempNum = num % 10;
+                num = num / 10;
+                sum += tempNum;
+            }
+            if (sum == 5 || sum == 7 || sum == 11)
+                System.out.println(i + " -> True");
+            else System.out.println(i + " -> False");
+        }
+
+    }
+}
