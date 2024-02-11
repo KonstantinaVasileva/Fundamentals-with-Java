e407f5a371721227c668571c4bd4279d860f5b3e
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-02-11 22:38:16 +0200

    Add files via upload

---
+++ MidExam/BlackFlag.java
@@ -0,0 +1,31 @@
+package MidExam;
+
+import java.util.Scanner;
+
+public class BlackFlag {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int days = Integer.parseInt(scanner.nextLine());
+        int dailyPlunder = Integer.parseInt(scanner.nextLine());
+        double expectedPlunder = Double.parseDouble(scanner.nextLine());
+        double totalPlunder = 0;
+
+        for (int day = 1; day <= days; day++) {
+            totalPlunder += dailyPlunder;
+            if (day % 3 == 0) {
+                totalPlunder += 0.5 * dailyPlunder;
+            }
+            if (day % 5 == 0){
+                totalPlunder *= 0.7;
+            }
+        }
+        if (totalPlunder >= expectedPlunder){
+            System.out.printf("Ahoy! %.2f plunder gained.", totalPlunder);
+        } else {
+            double percentPlunder = totalPlunder * 100/expectedPlunder;
+            System.out.printf("Collected only %.2f%% of the plunder.", percentPlunder);
+        }
+
+    }
+}
