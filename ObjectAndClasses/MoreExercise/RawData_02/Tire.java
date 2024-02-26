e414ec885373450f546b48febcbe9202da75d115
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-02-26 17:06:25 +0200

    Add files via upload

---
+++ ObjectAndClasses/MoreExercise/RawData_02/Tire.java
@@ -0,0 +1,19 @@
+package ObjectsAndClasses_MoreExercise.RawData_02;
+
+public class Tire {
+    public int age;
+    public double pressure;
+
+    public Tire(double pressure, int age) {
+        this.age = age;
+        this.pressure = pressure;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+}
