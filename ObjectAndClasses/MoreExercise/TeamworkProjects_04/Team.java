9b4cb554a920240d808ff20c7f8fa635bb2b6359
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-02-29 23:44:56 +0200

    Add files via upload

---
+++ ObjectAndClasses/MoreExercise/TeamworkProjects_04/Team.java
@@ -0,0 +1,27 @@
+package ObjectsAndClasses_MoreExercise.TeamworkProjects_04;
+
+import java.util.List;
+
+public class Team {
+public String teamName;
+public String creator;
+public List<String> users;
+
+    public Team(String teamName, String creator, List<String> users) {
+        this.teamName = teamName;
+        this.creator = creator;
+        this.users = users;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public List<String> getUsers() {
+        return users;
+    }
+}
